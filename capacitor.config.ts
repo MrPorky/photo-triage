04dc8a02600b4b96f5f47c0ab9da322497222d2b
❤MrPork❤ <5321067+MrPorky@users.noreply.github.com>
2025-11-15 06:17:41 +0100

    init capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'photo.triage',
+  appName: 'photo-triage',
+  webDir: 'dist'
+};
+
+export default config;
